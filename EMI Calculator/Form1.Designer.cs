3084f5759d45f1dab11cc78cb537660bc24a6dc2
RINKI RAY <rituray113@gmail.com>
2020-11-25 18:20:48 +0530

    first commit

---
+++ EMI Calculator/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace EMI_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtYears = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.txtLoan = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtinterest = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPayment = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtYears)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtYears);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtRate);
+            this.groupBox1.Controls.Add(this.txtLoan);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(518, 268);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loan Information";
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(216, 166);
+            this.txtYears.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.txtYears.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(278, 34);
+            this.txtYears.TabIndex = 4;
+            this.txtYears.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(216, 105);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(278, 34);
+            this.txtRate.TabIndex = 3;
+            // 
+            // txtLoan
+            // 
+            this.txtLoan.Location = new System.Drawing.Point(216, 49);
+            this.txtLoan.Name = "txtLoan";
+            this.txtLoan.Size = new System.Drawing.Size(278, 34);
+            this.txtLoan.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(22, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Years of Loan";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(22, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Interest Rate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(22, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Principal Amount";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txttotal);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtinterest);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtPayment);
+            this.groupBox2.Location = new System.Drawing.Point(13, 286);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(517, 363);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Payment Information";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(241, 209);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(250, 31);
+            this.txttotal.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(21, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 30);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Total Payment";
+            // 
+            // txtinterest
+            // 
+            this.txtinterest.Location = new System.Drawing.Point(241, 118);
+            this.txtinterest.Name = "txtinterest";
+            this.txtinterest.Size = new System.Drawing.Size(260, 31);
+            this.txtinterest.TabIndex = 8;
+            this.txtinterest.WordWrap = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(16, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(219, 30);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Total interest payable";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(21, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 30);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Monthly Payment (₹)";
+            // 
+            // txtPayment
+            // 
+            this.txtPayment.Location = new System.Drawing.Point(241, 44);
+            this.txtPayment.Name = "txtPayment";
+            this.txtPayment.Size = new System.Drawing.Size(260, 31);
+            this.txtPayment.TabIndex = 2;
+            this.txtPayment.TextChanged += new System.EventHandler(this.txtPayment_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 661);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "EMI Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtYears)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown txtYears;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.TextBox txtLoan;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPayment;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtinterest;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttotal;
+    }
+}
+
---
+++ EMI Calculator/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EMI_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Formula =p.r(1+r)n);
+        //calculate EMI method
+
+        static double EMI_Calculator(double dLoan, double dRate, double dYears)
+        {
+
+            double dPayment;
+            dRate = dRate / (12 * 100);
+            dYears = dYears * 12;
+
+
+            dPayment = (dLoan * dRate * (double)Math.Pow(1 + dRate, dYears)) / (double)(Math.Pow(1 + dRate, dYears) - 1);
+
+
+            return dPayment;
+            
+
+        }
+        static double Total_interest(double dLoan, double dRate, double dYears)
+        {
+            double dinterest;
+
+            dYears = dYears * 12;
+            dinterest = (dLoan * dRate * dYears)/100 ;
+          
+            return dinterest;  
+            
+
+
+        }
+        static double Total(double dLoan, double dRate)
+        {
+            double dtotal;
+
+
+
+            dRate = dRate / (12 * 100);
+            
+
+            dtotal = dLoan + dRate;
+
+            return dtotal;
+
+
+
+        }
+
+
+
+
+
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double dPayment;
+            double dPay;
+            double dinterest;
+            double dtotal;
+            
+            
+            double dLoan, dRate, dYears;
+            
+
+            dLoan = double.Parse(txtLoan.Text);
+            dRate = double.Parse(txtRate.Text);
+            dYears = double.Parse(txtYears.Text);
+            
+
+            dPayment = EMI_Calculator(dLoan, dRate,dYears);
+            
+            dPay = Math.Round(dPayment, 2);
+            dinterest = Total_interest(dLoan, dRate,dYears);
+            dtotal = Total(dLoan, dRate);
+
+
+
+
+            txtPayment.Text = " " + dPay ;
+            txtinterest.Text = " " + dinterest;
+            txttotal.Text = " " + dtotal;
+            txtPayment.Refresh();
+
+
+        }
+
+        private void txtPayment_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
